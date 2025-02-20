18ed5a9e6c6470aa85258b3eb083803334ef41a3
hashtaglifesux <131010413+hashtaglifesux@users.noreply.github.com>
2025-02-20 15:05:38 +0400

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,6 @@
+# when i get around to making it, this file will have main menu UI to select which game to play
 from games import pinball, pong
-# mancala
+# games to be added: 
+# mancala, game of life, loz-style thing, 
 
-pinball.play()
+pong.play()
